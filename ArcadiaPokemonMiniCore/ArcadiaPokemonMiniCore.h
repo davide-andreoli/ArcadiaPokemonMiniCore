2201bd0246e85ea17868031cd8f4ddccf022d091
Davide Andreoli <davide.andreoli@icloud.com>
2024-10-03 13:59:34 +0200

    Initial Commit

---
+++ ArcadiaPokemonMiniCore/ArcadiaPokemonMiniCore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaPokemonMiniCore.h
+//  ArcadiaPokemonMiniCore
+//
+//  Created by Davide Andreoli on 03/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaPokemonMiniCore.
+FOUNDATION_EXPORT double ArcadiaPokemonMiniCoreVersionNumber;
+
+//! Project version string for ArcadiaPokemonMiniCore.
+FOUNDATION_EXPORT const unsigned char ArcadiaPokemonMiniCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaPokemonMiniCore/PublicHeader.h>
+
+
---
+++ ArcadiaPokemonMiniCoreTests/ArcadiaPokemonMiniCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadiaPokemonMiniCoreTests.swift
+//  ArcadiaPokemonMiniCoreTests
+//
+//  Created by Davide Andreoli on 03/10/24.
+//
+
+import Testing
+@testable import ArcadiaPokemonMiniCore
+
+struct ArcadiaPokemonMiniCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
